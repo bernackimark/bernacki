022d2edb3d10fffbb34a63f39e623b439d3d7b7d
bernackimark@gmail.com <bernackimark@gmail.com>
2022-10-26 18:15:24 +0000

    Edited theme.css and ToDo

--- client_code/ToDo/__init__.py
+++ client_code/ToDo/__init__.py
@@ -17,23 +17,37 @@ class ToDo(ToDoTemplate):
     self.todo_dg.full_width_row = True
     self.header_gp.role, self.new_todo_gp.role, self.new_todo_group_gp.role = 'todo-gp', 'todo-gp', 'todo-gp'
     
-    self.load_header()
-    self.load_new_todo()
+    # Header Components
+    todo_btn = Button(text='To-do', icon='fa:calendar-plus-o', icon_align='left', tag='todo_btn')
+    todo_groups_lbl = Label(align='right', text='My To-do Groups', spacing_above='large', spacing_below='none')
+    todo_dd = DropDown(placeholder='...Add Group', items=['a','b'])
+    
+    # New To-do Components
+    new_todo_lbl = Label(text='New To-do *')
+    new_todo_tb = TextBox(width=200, tag='new_todo_name')
+    new_todo_existing_group_lbl = Label(text='Assign to To-do Group:')
+    new_todo_existing_group_dd = DropDown(width=200, tag='new_todo_group_name')
+    new_todo_cancel_btn = Button(text='Cancel', tag='new_todo_cancel_btn')
+    new_todo_ok_btn = Button(text='OK', tag='new_todo_ok_btn')
+    
+    
+    self.load_header(todo_btn, todo_groups_lbl, todo_dd)
+    self.load_new_todo(new_todo_lbl, new_todo_tb, new_todo_existing_group_lbl, new_todo_existing_group_dd, new_todo_cancel_btn, new_todo_ok_btn)
     self.load_new_todo_group()
     self.load_to_do_table()
     
-  def load_header(self):
-    self.header_gp.add_component(Button(text='To-do', icon='fa:calendar-plus-o', icon_align='left', tag='todo_btn'),row=1, col_xs=0, width_xs=2)
-    self.header_gp.add_component(Label(align='right', text='My To-do Groups', spacing_above='large', spacing_below='none'), row=1, col_xs=2, width_xs=2)
-    self.header_gp.add_component(DropDown(placeholder='...Add Group', items=['a','b']), row=1, col_xs=4, width_xs=3)
+  def load_header(self, todo_btn, todo_group_lbl, todo_dd):
+    self.header_gp.add_component(todo_btn, row=1, col_xs=0, width_xs=2)
+    self.header_gp.add_component(todo_group_lbl, row=1, col_xs=2, width_xs=2)
+    self.header_gp.add_component(todo_dd, row=1, col_xs=4, width_xs=3)
     
-  def load_new_todo(self):
-    self.new_todo_gp.add_component(Label(text='New To-do *'), row=1, col_xs=0, width_xs=3)
-    self.new_todo_gp.add_component(TextBox(width=200, tag='new_todo_name'), row=1, col_xs=3, width_xs=2)
-    self.new_todo_gp.add_component(Label(text='Assign to To-do Group:'), row=2, col_xs=0, width_xs=3)
-    self.new_todo_gp.add_component(DropDown(width=200, tag='new_todo_group_name'), row=2, col_xs=3, width_xs=3)
-    self.new_todo_gp.add_component(Button(text='Cancel', tag='new_todo_cancel_btn'), row=2, col_xs=6, width_xs=2)
-    self.new_todo_gp.add_component(Button(text='OK', tag='new_todo_ok_btn'), row=2, col_xs=7, width_xs=1)
+  def load_new_todo(self, new_todo_lbl, new_todo_tb, new_todo_existing_group_lbl, new_todo_existing_group_dd, new_todo_cancel_btn, new_todo_ok_btn):
+    self.new_todo_gp.add_component(new_todo_lbl, row=1, col_xs=0, width_xs=3)
+    self.new_todo_gp.add_component(new_todo_tb, row=1, col_xs=3, width_xs=2)
+    self.new_todo_gp.add_component(new_todo_existing_group_lbl, row=2, col_xs=0, width_xs=3)
+    self.new_todo_gp.add_component(new_todo_existing_group_dd, row=2, col_xs=3, width_xs=3)
+    self.new_todo_gp.add_component(new_todo_cancel_btn, row=2, col_xs=6, width_xs=2)
+    self.new_todo_gp.add_component(new_todo_ok_btn, row=2, col_xs=7, width_xs=1)
   
   def load_new_todo_group(self):
     self.new_todo_group_gp.add_component(Label(text='New To-do Group *'))
