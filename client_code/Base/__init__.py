0b3ef85ca345640234cd8168a20d18a1540271d3
bernackimark@gmail.com <bernackimark@gmail.com>
2022-10-26 17:20:37 +0000

    Created ToDo and ToDo.RowTemplate1; Edited settings, Base and standard-page.html

--- client_code/Base/__init__.py
+++ client_code/Base/__init__.py
@@ -10,12 +10,14 @@ from anvil.tables import app_tables
 from ..Home import Home
 from ..Setback import Setback
 from ..Setback.SetbackBotChallenge import SetbackBotChallenge
+from ..ToDo import ToDo
 
 class Base(BaseTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.my_stuff.width, self.sign_in.width = 130, 115
+    self.links_sidebar_cp.width = 100
     self.show_my_stuff_link()
     self.change_sign_in_text()
     # self.content_panel.add_component(Home())
@@ -33,7 +35,11 @@ class Base(BaseTemplate):
 
   def show_my_stuff_link(self):
     self.my_stuff.visible = anvil.users.get_user() != None
-  
+
+  def todo_link_click(self, **event_args):
+    self.content_panel.clear()
+    self.content_panel.add_component(ToDo())    
+    
   def sign_in_click(self, **event_args):
     user = anvil.users.get_user()
     if user:
@@ -53,7 +59,8 @@ class Base(BaseTemplate):
       self.sign_in.width = len(user['email']) * 20
     else:
       self.sign_in.text = "Sign In"
-    
+  
+
 
 
 
---
+++ client_code/ToDo/__init__.py
@@ -0,0 +1,48 @@
+from ._anvil_designer import ToDoTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class ToDo(ToDoTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    
+    self.header_gp.full_width_row, self.new_todo_gp.full_width_row, self.new_todo_group_gp.full_width_row = True, True, True
+    self.todo_dg.full_width_row = True
+    
+    self.load_header()
+    self.load_new_todo()
+    self.load_new_todo_group()
+    self.load_to_do_table()
+    
+  def load_header(self):
+    self.header_gp.add_component(Button(text='To-do', icon='fa:calendar-plus-o', icon_align='left', tag='todo_btn'),row=1, col_xs=0, width_xs=2)
+    self.header_gp.add_component(Label(align='right', text='My To-do Groups', spacing_above='large', spacing_below='none'), row=1, col_xs=2, width_xs=2)
+    self.header_gp.add_component(DropDown(placeholder='...Add Group', items=['a','b']), row=1, col_xs=4, width_xs=3)
+    
+  def load_new_todo(self):
+    self.new_todo_gp.add_component(Label(text='New To-do *'), row=1, col_xs=0, width_xs=3)
+    self.new_todo_gp.add_component(TextBox(width=200, tag='new_todo_name'), row=1, col_xs=3, width_xs=2)
+    self.new_todo_gp.add_component(Label(text='Assign to To-do Group:'), row=2, col_xs=0, width_xs=3)
+    self.new_todo_gp.add_component(DropDown(width=200), tag='new_todo_group_name')
+    self.new_todo_gp.add_component(Button(text='Cancel', tag='new_todo_cancel_btn'))
+    self.new_todo_gp.add_component(Button(text='OK', tag='new_todo_ok_btn'))
+  
+  def load_new_todo_group(self):
+    self.new_todo_group_gp.add_component(Label(text='New To-do Group *'))
+    self.new_todo_group_gp.add_component(TextBox(width=200, tag='new_todo_group_name'))
+    self.new_todo_group_gp.add_component(Button(text='Cancel', tag='new_todo_group_cancel_btn'))
+    self.new_todo_group_gp.add_component(Button(text='OK', tag='new_todo_group_ok_btn')) 
+    
+  def load_to_do_table(self):
+    self.todo_dg.columns=['A','B']
+    
+    
+    
+    
