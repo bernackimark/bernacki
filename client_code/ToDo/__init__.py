8e8e2234637009b63929cc4aa336ae8aad58f332
bernackimark@gmail.com <bernackimark@gmail.com>
2022-10-26 18:34:54 +0000

    Edited ToDo

--- client_code/ToDo/__init__.py
+++ client_code/ToDo/__init__.py
@@ -12,10 +12,14 @@ class ToDo(ToDoTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+
+    # dummy data
+    dummy_data = [{'a': 'Dummy A', 'b': 'Dummy B'}, {'a': 'Dummy A', 'b': 'Dummy B'}]    
     
     self.header_gp.full_width_row, self.new_todo_gp.full_width_row, self.new_todo_group_gp.full_width_row = True, True, True
-    self.todo_dg.full_width_row = True
+    self.todo_table_gp.full_width_row = True
     self.header_gp.role, self.new_todo_gp.role, self.new_todo_group_gp.role = 'todo-gp', 'todo-gp', 'todo-gp'
+    self.todo_table_gp.role = 'todo-gp'
     
     # Header Components
     todo_btn = Button(text='To-do', icon='fa:calendar-plus-o', icon_align='left', tag='todo_btn')
@@ -24,17 +28,27 @@ class ToDo(ToDoTemplate):
     
     # New To-do Components
     new_todo_lbl = Label(text='New To-do *')
-    new_todo_tb = TextBox(width=200, tag='new_todo_name')
+    new_todo_tb = TextBox(width=200)
     new_todo_existing_group_lbl = Label(text='Assign to To-do Group:')
-    new_todo_existing_group_dd = DropDown(width=200, tag='new_todo_group_name')
-    new_todo_cancel_btn = Button(text='Cancel', tag='new_todo_cancel_btn')
-    new_todo_ok_btn = Button(text='OK', tag='new_todo_ok_btn')
+    new_todo_existing_group_dd = DropDown(width=200)
+    new_todo_cancel_btn = Button(text='Cancel')
+    new_todo_ok_btn = Button(text='OK')
     
+    # New To-do Group Components
+    new_todo_group_lbl = Label(text='New To-do Group *')
+    new_todo_group_tb = TextBox(width=200)
+    new_todo_group_cancel_btn = Button(text='Cancel')
+    new_todo_group_ok_btn = Button(text='OK')
     
+    # To-do Table Components
+    todo_table_dg = DataGrid(columns=['a','b'])
+    todo_table_rp = RepeatingPanel(items=dummy_data)
+    
+    # Load all components
     self.load_header(todo_btn, todo_groups_lbl, todo_dd)
     self.load_new_todo(new_todo_lbl, new_todo_tb, new_todo_existing_group_lbl, new_todo_existing_group_dd, new_todo_cancel_btn, new_todo_ok_btn)
-    self.load_new_todo_group()
-    self.load_to_do_table()
+    self.load_new_todo_group(new_todo_group_lbl, new_todo_group_tb, new_todo_group_cancel_btn, new_todo_group_ok_btn)
+    self.load_to_do_table(todo_table_dg, todo_table_rp)
     
   def load_header(self, todo_btn, todo_group_lbl, todo_dd):
     self.header_gp.add_component(todo_btn, row=1, col_xs=0, width_xs=2)
@@ -47,16 +61,17 @@ class ToDo(ToDoTemplate):
     self.new_todo_gp.add_component(new_todo_existing_group_lbl, row=2, col_xs=0, width_xs=3)
     self.new_todo_gp.add_component(new_todo_existing_group_dd, row=2, col_xs=3, width_xs=3)
     self.new_todo_gp.add_component(new_todo_cancel_btn, row=2, col_xs=6, width_xs=2)
-    self.new_todo_gp.add_component(new_todo_ok_btn, row=2, col_xs=7, width_xs=1)
+    self.new_todo_gp.add_component(new_todo_ok_btn, row=2, col_xs=8, width_xs=1)
   
-  def load_new_todo_group(self):
-    self.new_todo_group_gp.add_component(Label(text='New To-do Group *'))
-    self.new_todo_group_gp.add_component(TextBox(width=200, tag='new_todo_group_name'))
-    self.new_todo_group_gp.add_component(Button(text='Cancel', tag='new_todo_group_cancel_btn'))
-    self.new_todo_group_gp.add_component(Button(text='OK', tag='new_todo_group_ok_btn')) 
+  def load_new_todo_group(self, new_todo_group_lbl, new_todo_group_tb, new_todo_group_cancel_btn, new_todo_group_ok_btn):
+    self.new_todo_group_gp.add_component(new_todo_group_lbl, row=1, col_xs=0, width_xs=3)
+    self.new_todo_group_gp.add_component(new_todo_group_tb, row=1, col_xs=3, width_xs=2)
+    self.new_todo_group_gp.add_component(new_todo_group_cancel_btn, row=2, col_xs=0, width_xs=2)
+    self.new_todo_group_gp.add_component(new_todo_group_ok_btn, row=2, col_xs=2, width_xs=1) 
     
-  def load_to_do_table(self):
-    self.todo_dg.columns=['A','B']
+  def load_to_do_table(self, todo_table_dg, todo_table_rp):
+    self.todo_table_gp.add_component(todo_table_dg)
+    todo_table_dg.add_component(todo_table_rp)
     
     
     
