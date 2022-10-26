cb3b3b7d930217fb85c7637e2939274a07cb910c
bernackimark@gmail.com <bernackimark@gmail.com>
2022-10-26 17:28:45 +0000

    Edited theme.css, theme/parameters.yaml and ToDo

--- client_code/ToDo/__init__.py
+++ client_code/ToDo/__init__.py
@@ -15,6 +15,7 @@ class ToDo(ToDoTemplate):
     
     self.header_gp.full_width_row, self.new_todo_gp.full_width_row, self.new_todo_group_gp.full_width_row = True, True, True
     self.todo_dg.full_width_row = True
+    self.header_gp.role, self.new_todo_gp.role, self.new_todo_group_gp.role = 'todo-gp', 'todo-gp', 'todo-gp'
     
     self.load_header()
     self.load_new_todo()
@@ -30,9 +31,9 @@ class ToDo(ToDoTemplate):
     self.new_todo_gp.add_component(Label(text='New To-do *'), row=1, col_xs=0, width_xs=3)
     self.new_todo_gp.add_component(TextBox(width=200, tag='new_todo_name'), row=1, col_xs=3, width_xs=2)
     self.new_todo_gp.add_component(Label(text='Assign to To-do Group:'), row=2, col_xs=0, width_xs=3)
-    self.new_todo_gp.add_component(DropDown(width=200), tag='new_todo_group_name')
-    self.new_todo_gp.add_component(Button(text='Cancel', tag='new_todo_cancel_btn'))
-    self.new_todo_gp.add_component(Button(text='OK', tag='new_todo_ok_btn'))
+    self.new_todo_gp.add_component(DropDown(width=200, tag='new_todo_group_name'), row=2, col_xs=3, width_xs=3)
+    self.new_todo_gp.add_component(Button(text='Cancel', tag='new_todo_cancel_btn'), row=2, col_xs=6, width_xs=2)
+    self.new_todo_gp.add_component(Button(text='OK', tag='new_todo_ok_btn'), row=2, col_xs=7, width_xs=1)
   
   def load_new_todo_group(self):
     self.new_todo_group_gp.add_component(Label(text='New To-do Group *'))
