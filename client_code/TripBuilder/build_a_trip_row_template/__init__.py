1e0917495c11c14df9764b737fdb0ed23f4caf66
bernackimark@gmail.com <bernackimark@gmail.com>
2023-06-25 02:47:06 +0000

    Edited TripBuilder.build_a_trip_row_template

--- client_code/TripBuilder/build_a_trip_row_template/__init__.py
+++ client_code/TripBuilder/build_a_trip_row_template/__init__.py
@@ -9,14 +9,16 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 import random
+from itertools import cycle
 
 POINT_NAME_PLACEHOLDERS = ['123 Main St, Anywhere, ST, USA', 'New Britain Museum of American Art', 'Charleston, SC',
                            'The Waldorf Astoria in New York City', '1600 Pennsylvania Avenue, Washington, DC, 37188', 'Dodger Stadium Los Angeles']
+next_placeholder = cycle(POINT_NAME_PLACEHOLDERS)
 
 class build_a_trip_row_template(build_a_trip_row_templateTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    self.tb_point_name.placeholder = POINT_NAME_PLACEHOLDERS[0]
-    POINT_NAME_PLACEHOLDERS.pop(0)
+    self.tb_point_name.placeholder = next(next_placeholder)
+    
