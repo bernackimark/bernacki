f2663f8304a4441fecfa4aa8e4d8b6bbadc8463f
bernackimark@gmail.com <bernackimark@gmail.com>
2023-06-26 18:04:48 +0000

    Edited geography.geocoding and geography

--- server_code/geography/geocoding.py
+++ server_code/geography/geocoding.py
@@ -5,10 +5,13 @@ BASE_URL_2 = "&format=json&apiKey="
 API_KEY = '8e11b4f1b14242409377a74e14aec790'
 
 
-def get_lat_long(locations_text: list[str]) -> list[tuple[float, float]]:
+def get_lat_long(locations: tuple[str, tuple]) -> list[tuple[float, float]]:
     lat_lon_list: list[tuple[float, float]] = []
-    for l in locations_text:
-        url = BASE_URL_1 + l + BASE_URL_2 + API_KEY
-        res = requests.get(url).json()['results'][0]
-        lat_lon_list.append((res['lat'], res['lon']))
+    for l in locations:
+        if l[1]:
+            lat_lon_list.append(l[1])  # if the lat_long is provided, skip the API and append to list
+        else:
+            url = BASE_URL_1 + l[0] + BASE_URL_2 + API_KEY
+            res = requests.get(url).json()['results'][0]
+            lat_lon_list.append((res['lat'], res['lon']))
     return lat_lon_list
