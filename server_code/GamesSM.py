1f418da18844eaac092d1384645dbe599b3a5a1e
bernackimark@gmail.com <bernackimark@gmail.com>
2023-07-25 04:56:20 +0000

    Edited GamesSM

--- server_code/GamesSM.py
+++ server_code/GamesSM.py
@@ -16,14 +16,17 @@ def write_game_data(d: dict, updated_player_data=dict()):
   if updated_player_data:
     user_email = d['player_emails'][0]
     user_row = app_tables.users.get(email=user_email)
-    print(updated_player_data)
-    print(user_row['info'])
+    user_row_info: dict = user_row['info']
     for k, v in updated_player_data.items():
-      print(k)
-      print(user_row['info'].keys())
-      if k in user_row['info'].keys():
-          user_row['info'][k] = v
-    print(user_row['info'])
+      if k in user_row_info.keys():
+          user_row_info[k] = v
+    user_row['info'] = user_row_info
+
+  # here is the reasoning behind this seemingly odd approach above
+  # row = app_tables.my_table.search()[0]
+  # dic = row['column_with_dict']
+  # dic['key'] = new_value  # row is not aware of this change
+  # row['column_with_dict'] = dic  # row is finally updated
 
   # remove any extra columns before writing a row to the games_log table
   db_columns = [d['name'] for d in app_tables.games_log.list_columns()]
