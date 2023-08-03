f95d5ae42c9a96a162bdaa2b84299c947f1e1a4c
bernackimark@gmail.com <bernackimark@gmail.com>
2023-08-03 04:21:21 +0000

    Created Utils; Edited settings and BaseballSM; Deleted UtilSM

--- server_code/BaseballSM.py
+++ server_code/BaseballSM.py
@@ -1,41 +1,89 @@
-import anvil.email
-import anvil.secrets
-import anvil.google.auth, anvil.google.drive, anvil.google.mail
-from anvil.google.drive import app_files
-import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 import anvil.server
-
+import anvil.email
 from bs4 import BeautifulSoup as bs
 import requests
 from datetime import date, timedelta
-from emailer import send_email
+from dataclasses import dataclass, field
 
 
 FG_STANDINGS_HEADERS = ['team_name', 'ytd_g', 'ytd_w', 'ytd_l', 'ytd_pct', 'ytd_run_diff', 'ytd_rs', 'ytd_ra',
                         'ros_g', 'ros_w', 'ros_l', 'ros_pct', 'ros_run_diff', 'ros_rs', 'ros_ra',
                         'proj_w', 'proj_l', 'proj_pct', 'proj_rdiff', 'proj_rs', 'proj_ra']
-SUBJECT_TEAMS = ['Braves', 'Rangers', 'Red Sox', 'Nationals', 'Athletics', 'Dodgers', 'Pirates']
-TEAM_NAME_ABBREVS = [('Dodgers', 'LAD'), ('Pirates', 'PIT'), ('Athletics', 'OAK'), ('Braves', 'ATL'),
-                     ('Rangers', 'TEX'), ('Red Sox', 'BOS'), ('Nationals', 'WSN')]
-NICKNAME_FULL_NAME = [('Dodgers', 'Los Angeles Dodgers'), ('Pirates', 'Pittsburgh Pirates'),
-                      ('Athletics', 'Oakland Athletics'), ('Braves', 'Atlanta Braves'),
-                      ('Rangers', 'Texas Rangers'), ('Red Sox', 'Boston Red Sox'), ('Nationals', 'Washington Nationals')]
 SUBJECT_COLUMNS = ['team_name', 'ytd_w', 'ytd_l', 'proj_w', 'proj_l']
 GAMES_IN_SEASON = 162
 
 yesterday = date.today() - timedelta(1)
 
-bets = [{'team_name': 'Dodgers', 'position': 'under', 'cnt': 96, 'amt': 5000},
-        {'team_name': 'Pirates', 'position': 'over', 'cnt': 67, 'amt': 5000},
-        {'team_name': 'Athletics', 'position': 'over', 'cnt': 59, 'amt': 20000},
-        {'team_name': 'Braves', 'position': 'under', 'cnt': 92, 'amt': 5000},
-        {'team_name': 'Rangers', 'position': 'over', 'cnt': 79.5, 'amt': 5000},
-        {'team_name': 'Red Sox', 'position': 'over', 'cnt': 76.5, 'amt': 5000},
-        {'team_name': 'Nationals', 'position': 'over', 'cnt': 59.5, 'amt': 5000},
-        {'team_name': 'Athletics', 'position': 'under', 'cnt': 58, 'amt': 5000}]
+
+@dataclass
+class Team:
+    name: str
+    full_name: str
+    code: str
+    wins: int = field(init=False)
+    losses: int = field(init=False)
+    yesterday_result: str = field(init=False)
+
+    @property
+    def games_played(self) -> int:
+        return self.wins + self.losses
+
+    @property
+    def games_remaining(self) -> int:
+        return GAMES_IN_SEASON - self.wins - self.losses
+
+
+@dataclass
+class OU:
+    team: Team
+    adverse: str
+    position: str
+    cnt: float
+    amt: int
+
+    @property
+    def amt_dollars(self) -> str:
+        return f'${round(self.amt/100)}'
+
+    @property
+    def magic_number(self) -> int:
+        num = round(self.cnt - self.team.wins) if self.position == 'o' else round(GAMES_IN_SEASON - self.cnt - self.team.losses)
+        return num
+
+    @property
+    def anti_magic_number(self) -> int:
+        return self.team.games_remaining - self.magic_number
+
+    @property
+    def outcome(self) -> str:
+        if self.magic_number <= 0:
+            return 'winner'
+        elif self.magic_number > self.team.games_remaining:
+            return 'loser'
+        else:
+            return 'active'
+
+    @property
+    def was_yesterday_good(self) -> bool:
+        if (self.team.yesterday_result == 'win' and self.position == 'o') or \
+           (self.team.yesterday_result == 'loss' and self.position == 'u'):
+            return True
+        return False
+
+    @property
+    def exclaim(self) -> str:
+        punctuation = '!' if self.was_yesterday_good else '.'
+        return punctuation
+
+
+bets = [OU(Team('Dodgers', 'Los Angeles Dodgers', 'LAD'), 'Vegas', 'u', 96, 5000),
+        OU(Team('Pirates', 'Pittsburgh Pirates', 'PIT'), 'Vegas', 'o', 67, 5000),
+        OU(Team('Athletics', 'Oakland Athletics', 'OAK'), 'Vegas', 'o', 59, 20000),
+        OU(Team('Braves', 'Atlanta Braves', 'ATL'), 'Jake', 'u', 92, 5000),
+        OU(Team('Rangers', 'Texas Rangers', 'TEX'), 'Jake', 'o', 79.5, 5000),
+        OU(Team('Red Sox', 'Boston Red Sox', 'BOS'), 'Jake', 'o', 76.5, 5000),
+        OU(Team('Nationals', 'Washington Nationals', 'WSN'), 'Jake', 'o', 59.5, 5000),
+        OU(Team('Athletics', 'Oakland Athletics', 'OAK'), 'Jake', 'o', 58, 5000)]
 
 
 def get_standings_html() -> list[str]:
@@ -51,39 +99,21 @@ def create_standings_from_html(html_table: list[str]) -> list[dict]:
             continue
         team_dict = dict()
         all_td_tags = row.find_all('td')
-        for idx, data in enumerate(all_td_tags):
+        for idx, data in enumerate(all_td_tags):  # this requires the table's column composition to remain constant
             team_dict[FG_STANDINGS_HEADERS[idx]] = data.string
         standings.append(team_dict)
     return standings
 
 
-def filter_teams(all_teams: list[dict], my_teams: list[str]) -> list[dict]:
-    return [team for team in all_teams if team['team_name'] in my_teams]
+def update_team_records(current_standings: list[dict]) -> None:
+    for bet in bets:
+        for team in current_standings:
+            if bet.team.name == team['team_name']:
+                bet.team.wins = int(team['ytd_w'])
+                bet.team.losses = int(team['ytd_l'])
 
 
-def filter_columns(current: list[dict]) -> list[dict]:
-    for d in current:
-        key_copy = list(d.keys())
-        for k in key_copy:
-            if k not in SUBJECT_COLUMNS:
-                del d[k]
-    return current
-
-
-def current_bet_stats(fg_teams: list[dict], my_bets: list[dict]) -> list[dict]:
-    current_stats: list[dict] = []
-    for b in my_bets:
-        for fg in fg_teams:
-            if b['team_name'] == fg['team_name']:
-                remaining_games = GAMES_IN_SEASON - int(fg['ytd_w']) - int(fg['ytd_l'])
-                remaining_wins_needed = b['cnt'] - int(fg['ytd_w'])
-                play_like_a = int(round((remaining_wins_needed / remaining_games) * 162, 0))
-                current_stats.append({'team_name': b['team_name'], 'wins_needed': remaining_wins_needed,
-                                      'remaining_games': remaining_games, 'must_play_like': play_like_a})
-    return current_stats
-
-
-def get_yesterday_results_stats_api() -> list[dict]:
+def get_yesterday_results_stats_api() -> list[[tuple[str, bool]]]:
     y = date.today() - timedelta(1)
     d: dict = requests.get(f'http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1&startDate={y}&endDate={y}').json()
     all_outcomes = []
@@ -91,36 +121,38 @@ def get_yesterday_results_stats_api() -> list[dict]:
         is_winner_away, is_winner_home = e['teams']['away'].get('isWinner'), e['teams']['home'].get('isWinner')
         all_outcomes.append((e['teams']['away']['team']['name'], is_winner_away))
         all_outcomes.append((e['teams']['home']['team']['name'], is_winner_home))
-    my_outcomes: list[dict] = []
-    for t in NICKNAME_FULL_NAME:
-        if t[1] not in [o[0] for o in all_outcomes]:
-            my_outcomes.append({'team_name': t[0], 'yesterday_result': 'Off day yesterday'})
-        for o in all_outcomes:
-            if t[1] == o[0]:
-                if o[1]:
-                    my_outcomes.append({'team_name': t[0], 'yesterday_result': 'They won yesterday'})
-                else:
-                    my_outcomes.append({'team_name': t[0], 'yesterday_result': 'They lost yesterday'})
-    return my_outcomes
+    return all_outcomes
 
 
-def format_text(fg_info: list[dict], bets: list[dict], bet_stats: list[dict], yesterday_results: list[dict]) -> str:
-    # stitch the four lists into just one
-    [bet.update(bet_stats[idx]) for idx, bet in enumerate(bets)]
-    [bet.update(fg_team) for bet in bets for fg_team in fg_info if fg_team['team_name'] == bet['team_name']]
-    [bet.update(res) for bet in bets for res in yesterday_results if res['team_name'] == bet['team_name']]
-    stitched_list = [f"Bet: {b['team_name']} {b['position']} {b['cnt']}. {b['yesterday_result']}. Their rest of the season line is: {b['wins_needed']}-{b['remaining_games'] - b['wins_needed']}. They must play like a {b['must_play_like']} win team." for b in bets]
-    return '\n'.join(stitched_list)
+def update_teams_yesterday_result(results: list[[tuple[str, bool]]]) -> None:
+    for b in bets:
+        if b.team.full_name not in [t[0] for t in results]:
+            b.team.yesterday_result = 'off'
+            continue
+        for res in results:
+            if b.team.full_name == res[0]:
+                b.team.yesterday_result = 'win' if res[1] else 'loss'
+
+
+def format_text() -> str:
+    text_list = [(f'{b.team.name} {b.amt_dollars} {b.adverse} bet. Yest: {b.team.yesterday_result}{b.exclaim}\nMagic # {b.magic_number}; Anti-magic #: {b.anti_magic_number}.', b.outcome) for b in bets]
+    active = [b[0] for b in text_list if b[1] == 'active']
+    winners = [b[0] for b in text_list if b[1] == 'winner']
+    losers = [b[0] for b in text_list if b[1] == 'loser']
+    active_text, winner_text, loser_text = '\n'.join(active), '\n'.join(winners), '\n'.join(losers)
+    return f'Active Bets:\n{active_text}\n\nWinners:\n{winner_text}\n\nLosers:{loser_text}'
 
 
 @anvil.server.callable
 @anvil.server.background_task
 def run_baseball_bets() -> None:
     html_table: list[str] = get_standings_html()
-    teams: list[dict] = create_standings_from_html(html_table)
-    filtered_teams: list[dict] = filter_teams(teams, SUBJECT_TEAMS)
-    fg_data: list[dict] = filter_columns(filtered_teams)
-    bet_stats: list[dict] = current_bet_stats(fg_data, bets)
+    teams_season_stats: list[dict] = create_standings_from_html(html_table)
+    update_team_records(teams_season_stats)
     yesterday_results: list[dict] = get_yesterday_results_stats_api()
-    message_body: str = format_text(fg_data, bets, bet_stats, yesterday_results)
-    send_email(from_name='Bernacki', to='bernackimark@gmail.com', subject='Baseball Bets', text=message_body)
+    update_teams_yesterday_result(yesterday_results)
+    message_body: str = format_text()
+    # send_yagmail('bernackimark@gmail.com', 'Baseball Bets', message_body)
+
+    # this line only works in Anvil
+    anvil.email.send(from_name = "Bernacki", to = "bernackimark@gmail.com", subject = "Baseball Bets", text = message_body)
