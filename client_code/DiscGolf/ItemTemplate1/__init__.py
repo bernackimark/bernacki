da855d582ce4d3705638a075c99572e031be41b2
bernackimark@gmail.com <bernackimark@gmail.com>
2023-09-10 18:00:34 +0000

    Edited DiscGolf.ItemTemplate1

--- client_code/DiscGolf/ItemTemplate1/__init__.py
+++ client_code/DiscGolf/ItemTemplate1/__init__.py
@@ -15,4 +15,7 @@ class ItemTemplate1(ItemTemplate1Template):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    self.img_player.source = m.get_image_url_from_name(self.lbl_leaderboard_value.text)
+    # self.img_player.source = m.get_image_url_from_name(self.lbl_leaderboard_value.text)
+    for p in m.dg_players:
+      if p['full_name'] == self.lbl_leaderboard_value.text:
+        self.img_player.source = p['photo']
